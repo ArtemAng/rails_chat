9c80d4f0cd71b829e22f9e3745f51e10793a83f6
ArtemAng <angalevichartem@gmail.com>
2022-12-30 16:51:45 +0300

    add devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # resources :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  
   root "rooms#index"
 
 end
